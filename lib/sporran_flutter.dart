4bc68474776e960c219d46d74eb019ea405c90e2
Nick Fisher <nick.fisher@avinium.com>
2019-08-22 13:54:57 +1000

    allow passing store externally

--- lib/sporran_flutter.dart
+++ lib/sporran_flutter.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/services.dart';
 import 'package:sporran/sporran_io.dart';
+import 'package:sporran/lawndart.dart';
 import 'package:sporran_flutter/sqflite_store.dart';
 
 class SporranFlutter {
@@ -12,11 +13,12 @@ class SporranFlutter {
   final String username;
   final String password;
   final String databaseName;
+  Stream<bool> connectivity;
 
   Sporran sporran;
   SporranInitialiser initialiser;
   
-  SporranFlutter(this.host, this.port, this.username, this.password, this.databaseName) {
+  SporranFlutter(this.host, this.port, this.username, this.password, this.databaseName, this.connectivity, [ Store store ]) {
     initialiser = new SporranInitialiser();
     initialiser.dbName = databaseName;
     initialiser.hostname = host;
@@ -26,13 +28,18 @@ class SporranFlutter {
     initialiser.username = username;
     initialiser.password = password;
     initialiser.preserveLocal = true;
+    initialiser.store = store;
   }
   
   void initialize() async {
-    final SqfliteStore store = await SqfliteStore.open("${initialiser.dbName}.db");
-    var online = Connectivity().onConnectivityChanged.map((x) => x != ConnectivityResult.none);
-    initialiser.store = store;
-    sporran = await getSporran(initialiser, online);
+    if(initialiser.store == null) {
+      initialiser.store = await SqfliteStore.open("${initialiser.dbName}.db");
+    }
+
+    if(connectivity == null) 
+      connectivity = Connectivity().onConnectivityChanged.map((x) => x != ConnectivityResult.none);
+    
+    sporran = await getSporran(initialiser, connectivity);
     sporran.autoSync = false;
   }
 

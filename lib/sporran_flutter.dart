d6f359a11ae75f9ab2f80526f5fb0de682577606
Nick Fisher <nick.fisher@avinium.com>
2019-08-24 13:03:15 +1000

    add localOnly switch

--- lib/sporran_flutter.dart
+++ lib/sporran_flutter.dart
@@ -5,19 +5,21 @@ import 'package:sporran/lawndart.dart';
 import 'package:sporran_flutter/sqflite_store.dart';
 
 class SporranFlutter {
-
   final String host;
   final String port;
   final String username;
   final String password;
   final String databaseName;
   final bool autoSync;
+  final bool localOnly;
   Stream<bool> connectivity;
 
   Sporran sporran;
   SporranInitialiser initialiser;
-  
-  SporranFlutter(this.host, this.port, this.username, this.password, this.databaseName, this.connectivity, { Store store, this.autoSync }) {
+
+  SporranFlutter(this.host, this.port, this.username, this.password,
+      this.databaseName, this.connectivity,
+      {Store store, this.autoSync, this.localOnly}) {
     initialiser = new SporranInitialiser();
     initialiser.dbName = databaseName;
     initialiser.hostname = host;
@@ -29,17 +31,18 @@ class SporranFlutter {
     initialiser.preserveLocal = true;
     initialiser.store = store;
   }
-  
+
   void initialize() async {
-    if(initialiser.store == null) {
+    if (initialiser.store == null) {
       initialiser.store = await SqfliteStore.open("${initialiser.dbName}.sql");
     }
 
-    if(connectivity == null) 
-      connectivity = Connectivity().onConnectivityChanged.map((x) => x != ConnectivityResult.none);
-    
-    sporran = await getSporran(initialiser, connectivity);
+    if (connectivity == null)
+      connectivity = Connectivity()
+          .onConnectivityChanged
+          .map((x) => x != ConnectivityResult.none);
+
+    sporran = await getSporran(initialiser, connectivity, localOnly);
     sporran.autoSync = autoSync;
   }
-
 }
